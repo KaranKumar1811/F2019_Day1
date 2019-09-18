723e45700a2f5c70b3dc3514a4d33a456192e917
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-18 19:32:37 -0400

    nothing

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args) {
+        System.out.println("Karan");
+    int a,b,c;
+    a=10;
+    b=20;
+    c=a+b;
+        System.out.println("Sum :"+ c);
+    }
+}
