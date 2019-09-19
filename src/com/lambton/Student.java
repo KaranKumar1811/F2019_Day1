5de4815a02fe3b319a576b3934cd27f9c6d37e51
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-18 20:28:29 -0400

    functions

--- src/com/lambton/Student.java
+++ src/com/lambton/Student.java
@@ -1,6 +1,18 @@
 package com.lambton;
 
+
+
 public class Student {
     int studentID;
     String studentname;
+    void setvalue()
+    {
+        studentID=1;
+        studentname="Karan Kumar";
+    }
+    void display()
+    {
+        System.out.println("Student ID :"+ studentID);
+        System.out.println("Student_Name :"+studentname);
+    }
 }
